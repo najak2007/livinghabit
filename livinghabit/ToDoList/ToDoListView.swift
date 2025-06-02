db35a9eb77833069a364c7a3e15bfd90f144c880
Najak <najak2007@gmail.com>
2025-06-02 18:35:30 +0900

    장소에 맞는 한일 목록만 나오도록 수정

--- livinghabit/ToDoList/ToDoListView.swift
+++ livinghabit/ToDoList/ToDoListView.swift
@@ -56,23 +56,19 @@ struct ToDoListView: View {
                     if fecthToSectionData(placeInfoData.alias) == true {
                         Section(header: Text(placeInfoData.alias)) {
                             ForEach(viewModel.toDoLists, id: \.id) { ToDoListData in
-                                VStack(alignment: .leading) {
-                                    Text(ToDoListData.toDoList)
-                                        .font(.custom("AppleSDGothicNeo-Medium", size: 18 ))
-                                        .foregroundColor(colorScheme == .dark ? Color(hex: "#FFFFFF") : Color(hex: "#000000"))
-#if false
-                                    Text("\(ToDoListData.date)")
-                                        .font(.custom("AppleSDGothicNeo-Regular", size: 15 ))
-                                        .foregroundColor(colorScheme == .dark ? Color(hex: "#FFFFFF") : Color(hex: "#000000"))
-#endif
-                                }
-                                .onTapGesture {
-                                    
-                                    self.endTextEditing()
-                                    
-                                    selectedToDoListData = ToDoListData
-                                    editedToDoList = ToDoListData.toDoList
-                                    showingCustomAlert = true
+                                if placeInfoData.alias == ToDoListData.placeInfoData?.alias {
+                                    VStack(alignment: .leading) {
+                                        Text(ToDoListData.toDoList)
+                                            .font(.custom("AppleSDGothicNeo-Medium", size: 18 ))
+                                            .foregroundColor(colorScheme == .dark ? Color(hex: "#FFFFFF") : Color(hex: "#000000"))
+                                    }
+                                    .onTapGesture {
+                                        self.endTextEditing()
+                                        
+                                        selectedToDoListData = ToDoListData
+                                        editedToDoList = ToDoListData.toDoList
+                                        showingCustomAlert = true
+                                    }
                                 }
                             }
                             .onDelete(perform: viewModel.deleteToDoList)
