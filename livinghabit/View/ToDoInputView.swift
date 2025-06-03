13c97d0695ea343cab64de174651de8a515cf651
najak2007 <>
2025-06-03 23:46:37 +0900

    ToDoList 에서 UI/UX 변경

--- livinghabit/View/ToDoInputView.swift
+++ livinghabit/View/ToDoInputView.swift
@@ -22,30 +22,31 @@ struct ToDoInputView: View {
                     .font(.custom("AppleSDGothicNeo-Medium", size: 24))
             })
             
-            
-            TextField("무엇을 할까?", text: $inputText)
-                .padding()
-                .focused($focusedField)
-                .font(.custom("AppleSDGothicNeo-Medium", size: 18))
-                .frame(height: 45)
-                .submitLabel(.done)
-                .onSubmit {
+            HStack {
+                TextField("무엇을 할까?", text: $inputText)
+                    .padding()
+                    .focused($focusedField)
+                    .font(.custom("AppleSDGothicNeo-Medium", size: 18))
+                    .frame(height: 45)
+                    .submitLabel(.done)
+                    .onSubmit {
 #if true
-                    
+                        
 #else
-                    let toDoListData = ToDoListData()
-                    toDoListData.toDoList = self.toDoList
-                    toDoListData.id = self.getToDoListDataID()
-                    toDoListData.placeInfoData = fetchToSelectedPlaceData()
-                    viewModel.saveToDoList(toDoListData)
-                    toDoList = ""
+                        let toDoListData = ToDoListData()
+                        toDoListData.toDoList = self.toDoList
+                        toDoListData.id = self.getToDoListDataID()
+                        toDoListData.placeInfoData = fetchToSelectedPlaceData()
+                        viewModel.saveToDoList(toDoListData)
+                        toDoList = ""
 #endif
-                }
+                    }
+            }
+            .background(RoundedRectangle(cornerRadius: 10)
+                .stroke(Color.blue.opacity(0.8), lineWidth: focusedField == false ? 0 : 1)
+                .fill(Color.gray.opacity(0.2) ))
+//            .padding(.horizontal, 10)
+//            .padding(.top, 5)
         }
-        .background(RoundedRectangle(cornerRadius: 10)
-            .stroke(Color.blue.opacity(0.8), lineWidth: focusedField == false ? 0 : 1)
-            .fill(Color.gray.opacity(0.2) ))
-        .padding(.horizontal, 10)
-        .padding(.top, 5)
     }
 }
